be729fad6c03a1ab84dd66553d269f2814d516ba
Pavel Hradecky <pavel.hradecky@cvut.cz>
2024-11-05 15:31:05 +0100

    Migrated from Redis database do InfluxDB. Adopted configuration to running in Podman. Changed configuration for telegraf, that is now bridge for getting messages from MQTT and populate data to database Influx.

--- src/views/LoadedPrograms.vue
+++ src/views/LoadedPrograms.vue
@@ -1,67 +1,68 @@
 <template>
-    <div>
-        <div cslass="card-header">
-            <div class="card-title py-3">Programs</div>
+  <div>
+    <div class="card-header">
+      <div class="card-title py-3">Programs</div>
+    </div>
+    <div class="dataTable-wrapper dataTable-loading no-footer fixed-columns">
+      <div class="dataTable-container block w-full overflow-x-auto whitespace-nowrap borderless hover">
+        <table class="table-3 dataTable-table max-w-full w-full">
+          <thead>
+            <tr>
+              <th class="text-left border-b pb-3 mb-3 text-gray-500 font-semibold">Name</th>
+              <th class="text-left border-b pb-3 mb-3 text-gray-500 font-semibold">Line</th>
+              <th class="text-left border-b pb-3 mb-3 text-gray-500 font-semibold">Start</th>
+              <th class="text-left border-b pb-3 mb-3 text-gray-500 font-semibold">End</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(program, index) in programs" :key="index">
+              <td class="text-xs py-5 px-4">
+                {{ program.name }}
+              </td>
+              <td class="text-xs">
+                {{ program.actual_line }}
+              </td>
+              <td class="py-5">
+                {{ $date(program.start).format('DD.MM.YYYY HH:mm:ss') }}
+              </td>
+              <td class="py-5">
+                {{ $date(program.end).format('DD.MM.YYYY HH:mm:ss') }}
+              </td>
+              <td class="py-5">
+                <BaseBtn rounded class="border border-primary text-primary hover:bg-primary hover:text-white">
+                  Detail
+                </BaseBtn>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="dataTable-bottom">
+        <div class="dataTable-info">
+          Total {{ programs.length }} entries
         </div>
-        <div class="dataTable-wrapper dataTable-loading no-footer fixed-columns">
-          <div class="dataTable-container block w-full overflow-x-auto whitespace-nowrap borderless hover">
-              <table class="table-3 dataTable-table max-w-full w-full">
-                  <thead>
-                      <tr class="">
-                          <th class=" text-left border-b pb-3 mb-3 text-gray-500 font-semibold">Name</th>
-                          <th class=" text-left border-b pb-3 mb-3 text-gray-500 font-semibold">Line</th>
-                          <th class=" text-left border-b pb-3 mb-3 text-gray-500 font-semibold">Start</th>
-                          <th class=" text-left border-b pb-3 mb-3 text-gray-500 font-semibold">End</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr v-for="(program, index) in programs" :key="index">
-                          <td class="text-xs py-5 px-4">
-                              {{program.name}}
-                          </td>
-                          <td class="text-xs">
-                              {{program.actual_line}}
-                          </td>
-                          <td class="py-5">
-                              {{ $date(program.start).format('DD.MM.YYYY HH:MM:ss') }}   
-                          </td>
-                          <td class="py-5">
-                              {{ $date(program.end).format('DD.MM.YYYY HH:MM:ss') }}        
-                          </td>
-                          <td class="py-5">
-                              <BaseBtn rounded class="border border-primary text-primary hover:bg-primary hover:text-white ">
-                                  Detail
-                              </BaseBtn>
-                          </td>
-                      </tr>
-                  </tbody>
-              </table>
-          </div>
-          <div class="dataTable-bottom">
-              <div class="dataTable-info">
-                  Total {{ programs.length }} entries
-              </div>
-              <nav class="dataTable-pagination">
-                  <ul class="dataTable-pagination-list"></ul>
-              </nav>
-          </div>
+        <nav class="dataTable-pagination">
+          <ul class="dataTable-pagination-list"></ul>
+        </nav>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
-  import { getLoadedPrograms } from '@/api/api';
-  
-  export default {
-    setup() {
-      const programs = ref([]);
-  
-      onMounted(async () => {
-        programs.value = await getLoadedPrograms();
-      });
-  
-      return { programs };
-    },
-  };
-  </script>
+    <!-- Optionally include the chart if accepted -->
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { getLoadedPrograms } from '@/api/api';
+
+export default {
+  setup() {
+    const programs = ref([]);
+
+    onMounted(async () => {
+      programs.value = await getLoadedPrograms();
+    });
+
+    return { programs };
+  },
+};
+</script>
